45c40fa975e675d6048754564d89f659e7fa3161
KausVlad <kausvirtual@gmail.com>
2023-06-24 02:44:21 +0300

    added about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,29 @@
 import { NavLink } from 'react-router-dom';
+import './About.scss';
 
 export function About() {
   return (
-    <div className="about-container">
-      <h1>About</h1>
-      <NavLink to="/">back</NavLink>
+    <div className="about-background">
+      <div className="about-container">
+        <h1>About</h1>
+        <div className="about">
+          <p className="about1">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+            dolore excepturi facere expedita iure eum. Placeat facere libero
+            iure ea atque, laboriosam corrupti odit repellat voluptatibus modi
+            sunt obcaecati maxime!
+          </p>
+          <img className="about-img1" src="public\images\1T.gif" alt="1" />
+          <p className="about2">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+            dolore excepturi facere expedita iure eum. Placeat facere libero
+            iure ea atque, laboriosam corrupti odit repellat voluptatibus modi
+            sunt obcaecati maxime!
+          </p>
+          <img className="about-img2" src="public\images\1T.gif" alt="1" />
+        </div>
+        <NavLink to="/">back</NavLink>
+      </div>
     </div>
   );
 }
