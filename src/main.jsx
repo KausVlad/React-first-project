6bac4e14e15366516b201cccd4ea485eac7d4685
KausVlad <kausvirtual@gmail.com>
2023-07-10 14:23:18 +0300

    strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { Provider } from 'react-redux';
 import { store } from './store/store.js';
 import { RouterProvider } from 'react-router-dom';
 import { Router } from './components/Router.jsx';
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <RouterProvider router={Router} />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={Router} />
+    </Provider>
+  </React.StrictMode>
 );
